a3b76167bbafbec1d47a115bc7bdfa803cde5914
Andrew Wong <andrew.j.wong@outlook.com>
2020-04-15 18:39:36 +1000

    Backup :: Wed 15 Apr 18:39:36 AEST 2020

--- kern/vm/__pagetable.c
+++ kern/vm/__pagetable.c
@@ -1,6 +1,13 @@
 #include <__pagetable.h>
+#include <types.h>
 
-int pagetable_translate((void*) address) {
-    int offset = 
+int pagetable_translate(int32_t * address) {
+    int32_t _address = (int32_t) address;
 
+    int offset = _address & 0xFFF;
+    int index = (_address >>= 12) & 0x3FF;
+    int table_index = (_address >>= 10) & 0x1FF;
+    int isKernelAddress = (_address >>= 9) & 1;
+
+    return 0;
 }
