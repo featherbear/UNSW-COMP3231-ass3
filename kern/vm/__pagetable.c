674ed79297cb80f51dedd7cc9bd27694f8376acb
Andrew Wong <andrew.j.wong@outlook.com>
2020-04-15 18:49:36 +1000

    Backup :: Wed 15 Apr 18:49:36 AEST 2020

--- kern/vm/__pagetable.c
+++ kern/vm/__pagetable.c
@@ -4,10 +4,24 @@
 int pagetable_translate(int32_t * address) {
     int32_t _address = (int32_t) address;
 
-    int offset = _address & 0xFFF;
-    int index = (_address >>= 12) & 0x3FF;
+    // VIRTUAL_MEMORY_ADDRESS
+    //   |       |       \ 
+    //   |       \        12 bits - offset
+    //   \       10 bits - level 2 offset
+    //    10 bits - level 1 offset
+
+
+
+    // FIXME: Check for Kernel address?
+
+    int offset = _address & 0xFFF; // Last 12 bits
+    int index = (_address >>= 12) & 0x3FF; // 
     int table_index = (_address >>= 10) & 0x1FF;
     int isKernelAddress = (_address >>= 9) & 1;
 
+
+    // TODO: Do lookup
+
+    // return (lookup << 20) | offset;
     return 0;
 }
