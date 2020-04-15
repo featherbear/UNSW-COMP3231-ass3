025f8787bba0a47eb290c9f4334d125428e15862
Andrew Wong <andrew.j.wong@outlook.com>
2020-04-15 18:29:36 +1000

    Backup :: Wed 15 Apr 18:29:36 AEST 2020

---
+++ kern/include/__pagetable.h
@@ -0,0 +1,5 @@
+#ifndef _PAGETABLE_H_
+#define _PAGETABLE_H_
+
+
+#endif /* _PAGETABLE_H_ */
---
+++ kern/vm/__pagetable.c
@@ -0,0 +1,6 @@
+#include <__pagetable.h>
+
+int pagetable_translate((void*) address) {
+    int offset = 
+
+}
